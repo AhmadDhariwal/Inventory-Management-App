0e70a73dfe5d2e22fcbf2dac341f9aefaadf790f
Ahmad Hassan <ahmaddhariwal023.com>
2026-01-14 15:35:53 +0500

    start frontend

---
+++ frontend/src/app/stock/stock-report/stock-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-report.component.html',
+  styleUrl: './stock-report.component.scss'
+})
+export class StockReportComponent {
+
+}
