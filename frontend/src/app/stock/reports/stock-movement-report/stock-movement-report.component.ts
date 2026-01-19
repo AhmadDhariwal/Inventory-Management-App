a82b950d7ea83e5c722035f800d1eb1a7c0bdff0
Ahmad Hassan <ahmaddhariwal023.com>
2026-01-19 14:34:48 +0500

    auto-commit

---
+++ frontend/src/app/stock/reports/stock-movement-report/stock-movement-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-movement-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-movement-report.component.html',
+  styleUrl: './stock-movement-report.component.scss'
+})
+export class StockMovementReportComponent {
+
+}
---
+++ frontend/src/app/stock/reports/stock-summary/stock-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './stock-summary.component.html',
+  styleUrl: './stock-summary.component.scss'
+})
+export class StockSummaryComponent {
+
+}
